e479e487c9d476fff37cba27026e2798086ba6dc
0xBooper <yourhighboopness@gmail.com>
2021-04-06 22:16:18 +0000

    Fixed uninstaller not working and added a check

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 echo "Uninstalling..."
-echo "This may require sudo priviledges."
-sudo rm /usr/bin/bashutil
-sudo cd /usr/bin
-[ -x "bashutil" ] && echo "Bashutil failed to be removed"; exit 1
-[ ! -x "bashutil" ] && echo "Bashutil successfully been removed."; exit 0
+rm /usr/bin/bashutil
+cd /usr/bin
+if [ -x bashutil ]; then
+	echo "Bashutil successfully been removed."
+	cd ~
+	exit 0
+else
+	echo "Bashutil failed to be removed"
+	cd ~
+	exit 1
+fi
